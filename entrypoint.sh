ab8429bf26aef95d69ac48ed8e90956260e2552e
wakusei-meron- <b0941015+gihub@gmail.com>
2021-04-15 19:14:08 +0900

    fix: push

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,42 @@ git config user.email "${GITHUB_EMAIL:-redash-versioning-queries@users.noreply.g
 echo "export queries..."
 python query_export.py --redash-url "${INPUT_ADDRESS}" --api-key "${INPUT_API_KEY}"
 
+
 git add .
 
 if [ -n "$(git diff --cached --name-only)" ]; then
-  current_branch=${GITHUB_REF#refs/heads/}
-  echo ${current_branch}
+  INPUT_FORCE=${INPUT_FORCE:-false}
+  INPUT_TAGS=${INPUT_TAGS:-false}
+  INPUT_DIRECTORY=${INPUT_DIRECTORY:-'.'}
+  _FORCE_OPTION=''
+  REPOSITORY=${INPUT_REPOSITORY:-$GITHUB_REPOSITORY}
 
-  remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-  git commit -m "update: $(date +%FT%R)"
-  git pull
-  git push
+  echo "Push to branch $INPUT_BRANCH";
+  [ -z "${INPUT_GITHUB_TOKEN}" ] && {
+      echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+      exit 1;
+  };
+
+  if ${INPUT_FORCE}; then
+      _FORCE_OPTION='--force'
+  fi
+
+  if ${INPUT_TAGS}; then
+      _TAGS='--tags'
+  fi
+
+  cd ${INPUT_DIRECTORY}
+
+  remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
+
+  git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags $_FORCE_OPTION $_TAGS;
+
+  # current_branch=${GITHUB_REF#refs/heads/}
+  # echo ${current_branch}
+
+  # git rebase
+  # remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+  # git commit -m "update: $(date +%FT%R)"
+  # git push
 fi
 
